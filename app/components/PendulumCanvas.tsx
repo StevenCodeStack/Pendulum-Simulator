027134affa0d40fc778310e688aac595b485d26a
StevenCodeStack <steven.codestack@gmail.com>
2026-07-06 07:32:47 +0800

    Simulating a pendulum testing

---
+++ app/components/PendulumCanvas.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useRef, useEffect } from "react";
+
+export default function PendulumCanvas() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    if (canvas.current) {
+      const el = canvas.current;
+      const ctx = el.getContext("2d")!;
+      const rect = el.getBoundingClientRect();
+      const dpr = window.devicePixelRatio;
+      el.width = rect.width * dpr;
+      el.height = rect.height * dpr;
+      ctx.scale(dpr, dpr);
+      const dt = 1 / 120;
+
+      const g = 800;
+      const pivotX = el.width / 2;
+      const pivotY = 50;
+      const length = 600;
+
+      let angle = Math.PI / 2;
+      let angularSpeed = 0;
+
+      function update() {
+        ctx.clearRect(0, 0, rect.width, rect.height);
+
+        // Logic
+        const angularAcceleration = -(g / length) * Math.sin(angle);
+        angularSpeed = angularSpeed + angularAcceleration * dt;
+        angle = angle + angularSpeed * dt;
+
+        const xDisplacement = length * Math.sin(angle);
+        const yDisplacement = length * Math.cos(angle);
+        const massX = el.width / 2 + xDisplacement;
+        const massY = pivotY + yDisplacement;
+
+        // Drawing
+        ctx.beginPath();
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "#0f3460";
+        ctx.moveTo(pivotX, pivotY);
+        ctx.lineTo(massX, massY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(pivotX, pivotY, 20, 0, Math.PI * 2);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.arc(massX, massY, 30, 0, Math.PI * 2);
+        ctx.fillStyle = "red";
+        ctx.fill();
+
+        requestAnimationFrame(update);
+      }
+
+      update();
+    }
+  }, []);
+
+  return <canvas ref={canvas} className="h-full w-full bg-slate-200"></canvas>;
+}
---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-100 px-5 py-2 absolute top-0 left-0 h-full bg-slate-500">
+      Pendulum Calculator
+    </div>
+  );
+};
+
+export default Sidebar;
